7c1eb42b54a5958b314478d22ee936f699db5431
Garrett <ga.landin89@gmail.com>
2022-03-02 16:09:01 -0600

    Created customer.py, product.py, shopping_cart.py and started working on them

---
+++ customer.py
@@ -0,0 +1,6 @@
+
+class Customer
+
+    def __int__(self, name):
+        self.name = name
+        self.shopping_cart = ""
---
+++ shopping_cart.py
@@ -0,0 +1,18 @@
+
+from ensurepip import version
+
+
+class ShoppingCart
+
+    def __init__(self):
+        self.cart = []
+
+    def number_of_products():
+        total_number_of_products = len(self.cart)
+        return total_number_of_products
+
+    def add_items_to_cart(product):
+        self.cart.append(product)
+
+    def remove_all_from_cart():
+        del self.cart
