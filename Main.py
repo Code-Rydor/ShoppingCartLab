cec517b400b79cbb60d39f78ed8e61482e0b7220
Garrett <ga.landin89@gmail.com>
2022-03-02 17:01:38 -0600

    Made some tweaks trying to bring it all together

---
+++ Main.py
@@ -0,0 +1,7 @@
+from product import Product
+from customer import Customer
+from shopping_cart import ShoppingCart
+
+my_cart = ShoppingCart
+
+customer1 = Customer("Garrett", my_cart)
--- customer.py
+++ customer.py
@@ -1,6 +1,10 @@
+from shopping_cart import ShoppingCart
 
 class Customer:
 
-    def __int__(self, name):
+    def __int__(self, name, cart_passed_in):
         self.name = name
-        self.shopping_cart = ""
+        self.shopping_cart = cart_passed_in
+
+    def add_product_to_cart(self, add_items_to_cart()):
+        #Still working on this 
